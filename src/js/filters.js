4df30a3362793191a2e71ea4caf3446d4d694e3c
Artem Safin <artemsafin.webdev@gmail.com>
2025-07-22 17:09:07 +0300

    По каталогу

--- src/js/filters.js
+++ src/js/filters.js
@@ -1,8 +1,3 @@
-import { MOBILE_WIDTH } from './constants';
-import { lockScroll, unlockScroll } from './scrollBlocker';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import gsap from 'gsap';
-
 export default function filters() {
     const catalogFilters = document.querySelector('.js-catalog-filters');
 
@@ -14,19 +9,11 @@ export default function filters() {
         const openDropdown = () => {
             btn.classList.toggle('active');
             dropdown.classList.toggle('active');
-
-            if (window.matchMedia(`(max-width: ${MOBILE_WIDTH}px)`).matches) {
-                lockScroll(dropdown);
-            }
         };
 
         const closeDropdown = () => {
             btn.classList.remove('active');
             dropdown.classList.remove('active');
-
-            if (window.matchMedia(`(max-width: ${MOBILE_WIDTH}px)`).matches) {
-                unlockScroll();
-            }
         };
 
         btn.addEventListener('click', event => {
@@ -38,50 +25,24 @@ export default function filters() {
             btn.addEventListener('click', event => {
                 event.preventDefault();
                 closeDropdown();
-            })
-        })
+            });
+        });
 
         document.addEventListener('click', event => {
             if (event.target.matches('.js-catalog-filters') || event.target.closest('.js-catalog-filters')) return;
             closeDropdown();
         });
 
-        if (window.matchMedia(`(max-width: ${MOBILE_WIDTH}px)`).matches) {
-            const SPEED = 0.5;
+        const elements = Array.from(document.querySelectorAll('.catalog__filters-accordion'));
 
-            const openAccordion = element => {
-                gsap.to(element, {
-                    height: 'auto',
-                    duration: SPEED,
-                    onComplete: () => ScrollTrigger.refresh()
-                    
-                });
-            };
-            const closeAccordion = element => {
-                gsap.to(element, {
-                    height: 0,
-                    duration: SPEED,
-                    onComplete: () => ScrollTrigger.refresh()
-                });
-            };
+        elements.forEach(element => {
+            const btn = element.querySelector('.catalog__filters-accordion-btn');
+            const content = element.querySelector('.catalog__filters-accordion-content');
 
-            const elements = Array.from(document.querySelectorAll('.catalog__filters-accordion'));
-
-            elements.forEach(element => {
-                const btn = element.querySelector('.catalog__filters-accordion-btn');
-                const content = element.querySelector('.catalog__filters-accordion-content');
-
-                btn.addEventListener('click', event => {
-                    event.preventDefault();
-
-                    if (element.classList.contains('active')) {
-                        closeAccordion(content);
-                    } else {
-                        openAccordion(content);
-                    }
-                    element.classList.toggle('active');
-                });
+            btn.addEventListener('click', event => {
+                event.preventDefault();
+                content.classList.toggle('active');
             });
-        }
+        });
     }
 }
